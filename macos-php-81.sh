2ddc6993a4ab13f87f81017caaf71b54a0133edc
Amoyensis <amoyensis@outlook.com>
2024-11-12 19:37:20 +0800

    Update macos-php-83.sh

--- macos-php-81.sh
+++ macos-php-81.sh
@@ -4,18 +4,18 @@ brew install wget autoconf automake libtool re2c bison pkg-config openssl libico
 
 base_dir=$(cd "$(dirname "$0")";pwd)
 
-sudo rm -rf ${base_dir}"/macos-build/php81"
+sudo rm -rf ${base_dir}"/macos-build/php83"
 sudo rm -rf /tmp/pecl/install
 sudo rm -rf /tmp/pear/install
-mkdir -p ${base_dir}"/macos-build/php81"
-cd ${base_dir}"/macos-build/php81" || exit
+mkdir -p ${base_dir}"/macos-build/php83"
+cd ${base_dir}"/macos-build/php83" || exit
 
-wget https://www.php.net/distributions/php-8.1.27.tar.gz || exit
-tar -xzvf php-8.1.27.tar.gz
+wget https://www.php.net/distributions/php-8.3.13.tar.gz || exit
+tar -xzvf php-8.3.13.tar.gz
 
-cd "php-8.1.27" || exit
+cd "php-8.3.13" || exit
 
-./configure --prefix="${base_dir}"/macos-build/php81/php \
+./configure --prefix="${base_dir}"/macos-build/php83/php \
   --with-external-pcre=$(brew --prefix pcre2) \
   --with-openssl=$(brew --prefix openssl) \
   --with-iconv=$(brew --prefix libiconv) \
@@ -39,16 +39,16 @@ cd "php-8.1.27" || exit
 make -j $(sysctl -n hw.ncpu)
 make install
 
-cp php.ini-development ${base_dir}/macos-build/php81/php/lib/php.ini
-export PATH=${base_dir}"/macos-build/php81/php/bin:$PATH"
+cp php.ini-development ${base_dir}/macos-build/php83/php/lib/php.ini
+export PATH=${base_dir}"/macos-build/php83/php/bin:$PATH"
 
-cd "${base_dir}/macos-build/php81" || exit
+cd "${base_dir}/macos-build/php83" || exit
 wget https://github.com/swoole/swoole-src/archive/refs/tags/v5.1.2.zip
 unzip v5.1.2.zip
 cd "swoole-src-5.1.2" || exit
-"${base_dir}"/macos-build/php81/php/bin/phpize
+"${base_dir}"/macos-build/php83/php/bin/phpize
 
-ln -s $(brew --prefix pcre2)/include/pcre2.h "${base_dir}"/macos-build/php81/php/include/php/ext/pcre/pcre2.h
+ln -s $(brew --prefix pcre2)/include/pcre2.h "${base_dir}"/macos-build/php83/php/include/php/ext/pcre/pcre2.h
 ./configure \
   --enable-openssl \
   --with-openssl-dir=$(brew --prefix openssl) \
@@ -56,31 +56,31 @@ ln -s $(brew --prefix pcre2)/include/pcre2.h "${base_dir}"/macos-build/php81/php
   --enable-swoole-curl \
   --enable-swoole-json \
   --enable-thread-context \
-  --with-php-config=${base_dir}/macos-build/php81/php/bin/php-config
+  --with-php-config=${base_dir}/macos-build/php83/php/bin/php-config
 
 make -j $(sysctl -n hw.ncpu)
 make install
 
-cd "${base_dir}"/macos-build/php81
+cd "${base_dir}"/macos-build/php83
 wget http://pear.php.net/go-pear.phar
-sudo ${base_dir}/macos-build/php81/php/bin/php go-pear.phar
-sudo ${base_dir}/macos-build/php81/php/bin/pear config-get php_dir
+sudo ${base_dir}/macos-build/php83/php/bin/php go-pear.phar
+sudo ${base_dir}/macos-build/php83/php/bin/pear config-get php_dir
 
-sudo ${base_dir}/macos-build/php81/php/bin/pecl channel-update pecl.php.net
-sudo ${base_dir}/macos-build/php81/php/bin/pecl install redis
+sudo ${base_dir}/macos-build/php83/php/bin/pecl channel-update pecl.php.net
+sudo ${base_dir}/macos-build/php83/php/bin/pecl install redis
 
-echo "memory_limit=1G" >> "${base_dir}"/macos-build/php81/php/lib/php.ini
-echo "opcache.enable_cli = 'On'" >> "${base_dir}"/macos-build/php81/php/lib/php.ini
-echo "extension=redis.so" >> "${base_dir}"/macos-build/php81/php/lib/php.ini
-echo "extension=swoole.so" >> "${base_dir}"/macos-build/php81/php/lib/php.ini
-echo "swoole.use_shortname = 'Off'" >> "${base_dir}"/macos-build/php81/php/lib/php.ini
+echo "memory_limit=1G" >> "${base_dir}"/macos-build/php83/php/lib/php.ini
+echo "opcache.enable_cli = 'On'" >> "${base_dir}"/macos-build/php83/php/lib/php.ini
+echo "extension=redis.so" >> "${base_dir}"/macos-build/php83/php/lib/php.ini
+echo "extension=swoole.so" >> "${base_dir}"/macos-build/php83/php/lib/php.ini
+echo "swoole.use_shortname = 'Off'" >> "${base_dir}"/macos-build/php83/php/lib/php.ini
 
-"${base_dir}"/macos-build/php81/php/bin/php -v
-"${base_dir}"/macos-build/php81/php/bin/php -m
-"${base_dir}"/macos-build/php81/php/bin/php --ri swoole
+"${base_dir}"/macos-build/php83/php/bin/php -v
+"${base_dir}"/macos-build/php83/php/bin/php -m
+"${base_dir}"/macos-build/php83/php/bin/php --ri swoole
 
-cd ${base_dir}/macos-build/php81 || exit
+cd ${base_dir}/macos-build/php83 || exit
 wget https://mirrors.aliyun.com/composer/composer.phar
-"${base_dir}"/macos-build/php81/php/bin/php composer.phar
+"${base_dir}"/macos-build/php83/php/bin/php composer.phar
 
 echo -e "\033[42;37m Build Completed :).\033[0m\n"
